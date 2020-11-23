5a99374d601e558a51bec56bb8a5e6adbaf515c6
bobsingor <bob@singor.com>
2020-11-23 13:36:55 +0200

    Update index.js

--- components/Head/index.js
+++ components/Head/index.js
@@ -5,11 +5,15 @@ import Header from 'next/head'
 const Head = ({ data }) => {
   return (
     <Header>
-      {data.meta_title && <title>{data.meta_title}</title>}
-      {data.meta_description && <meta name="description" content={data.meta_description} />}
-      {data.meta_title && <meta name="og:title" content={data.meta_title} />}
-      {data.meta_description && <meta name="og:description" content={data.meta_description} />}
-      {data.og_image && <meta property="og:image" content={data.og_image.url} />}
+      {data &&
+        <>
+          {data.meta_title && <title>{data.meta_title}</title>}
+          {data.meta_description && <meta name="description" content={data.meta_description} />}
+          {data.meta_title && <meta name="og:title" content={data.meta_title} />}
+          {data.meta_description && <meta name="og:description" content={data.meta_description} />}
+          {data.og_image && <meta property="og:image" content={data.og_image.url} />}
+        </>
+      }
     </Header>
   )
 }
